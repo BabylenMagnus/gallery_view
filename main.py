a0a368a06c94053b0ce1f22dfc567630f128cc42
Evgeniy Sevostyanihin <sevostyanikhin02@gmail.com>
2023-09-01 09:44:28 +0300

    changes

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
 import os
+import pandas as pd
 import numpy as np
 import cv2
 
 import gradio as gr
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 import glob
+import json
 
 
 RESULT_PATH = "/root/inpaint_api/results/"
 ORIG_IMG = "/root/new/"
+
+# RESULT_PATH = "/home/jjjj/Pictures/inpaint_api/results/"
+# ORIG_IMG = "/home/jjjj/Documents/new/"
+
+BBOX_PATH = "/home/jjjj/Pictures/without/bboxes.json"
+
+UP_FONT_PATH = "PassionOne-Regular.ttf"
+DOWN_FONT_PATH = "RedHatText-VariableFont_wght.ttf"
+
+
+with open(BBOX_PATH, "r") as t:
+    BBOXES = json.load(t)
+
 
 save_path = "saves/"
 
@@ -33,8 +48,48 @@ def change_dir(num, button=None):
     return get_imgs(num), num, "# " + RESULT_DIRS[int(num)]
 
 
-def select_image(evt: gr.SelectData, gallery):
-    return Image.open(gallery[evt.index]['name'])
+def select_image(evt: gr.SelectData, gallery, num):
+    bb = BBOXES[RESULT_DIRS[int(num)]].copy()
+    bb["height"] = [b - t for b, t in zip(bb["bottom"], bb["top"])]
+    del bb["right"]
+    del bb["left"]
+    del bb["bottom"]
+    bb["text"] = [i.upper() for i in bb["text"]]
+    bb["up_font"] = [1 for _ in bb["text"][:-1]] + [0]
+
+    return (
+        Image.open(os.path.join(ORIG_IMG, RESULT_DIRS[int(num)] + ".jpg")),
+        Image.open(gallery[evt.index]['name']),
+        Image.open(gallery[evt.index]['name']), pd.DataFrame(bb))
+
+
+def add_text_font(img, result):
+    img = Image.fromarray(img)
+
+    for j in range(len(result)):
+        j = result.loc[j]
+
+        image = Image.new('RGB', img.size, "white")
+        draw = ImageDraw.Draw(image)
+        font = ImageFont.truetype(UP_FONT_PATH if j["up_font"] else DOWN_FONT_PATH, int(j["height"]))
+        _, _, w, h = draw.textbbox(
+            (0, 0), j["name"], font=font
+        )
+
+        draw = ImageDraw.Draw(img)
+        draw.text(((img.size[0] - w) / 2, int(j["top"])), j["name"], font=font, fill="white")
+
+        # image = Image.new('L', (int(w * 2), int(h * 2)), "white")
+        #
+        # draw = ImageDraw.Draw(image)
+        # myFont = ImageFont.truetype("/home/jjjj/Downloads/Blacknorthdemo-mLE25.otf", size=h)
+        # _, _, w_, h_ = draw.textbbox((0, 0), j["name"], font=myFont)
+        # draw.text((0, 0), j["name"], font=myFont, fill="black")
+        # text_ = np.array(image)[:h_, :w_]
+        # text_ = Image.fromarray(text_).resize((int(w), int(h)), reducing_gap=3.0)
+        # img.paste(text_, (j["left"], j["bottom"]), mask=Image.fromarray(255 - np.array(text_)))
+
+    return img
 
 
 def load_img_to_array(img_p):
@@ -80,6 +135,7 @@ def add_text(img, page, thickness, threshold):
 def save_img(img, page):
     img = Image.fromarray(img)
     name = RESULT_DIRS[int(page)]
+    print("save", name)
     img.save(save_path + name + str(len(glob.glob(save_path + name + "*.png"))) + ".png")
 
 
@@ -103,16 +159,22 @@ with gr.Blocks() as demo:
 
         with gr.TabItem("Add text"):
             with gr.Row():
+                real_img = gr.Image(height=800, show_download_button=True)
                 image = gr.Image(height=800, show_download_button=True)
                 with gr.Column():
-                    # result = gr.Dataframe(
-                    #     headers=["name", "left", "right", "top", "bottom"],
-                    #     datatype=["str", "number", "number", "number", "number"]
-                    # )
+                    result = gr.Dataframe(
+                        headers=["name", "top", "height", "up_font"],
+                        datatype=["str", "number", "number", "number"]
+                    )
                     threshold = gr.Number(value=195, minimum=150, maximum=250, label="threshold")
                     thickness = gr.Number(value=3, minimum=0, maximum=15, label="thickness")
+
                     add_text_to_image = gr.Button("add text", variant="primary")
+                    add_text_from_pont = gr.Button("add text own font", variant="primary")
                     save_button = gr.Button("Save image", variant="primary")
+
+        with gr.TabItem("Hide"):
+            image_orig = gr.Image(height=800, show_download_button=True)
 
     page.submit(
         change_dir,
@@ -134,13 +196,13 @@ with gr.Blocks() as demo:
 
     gallery.select(
         select_image,
-        [gallery],
-        [image]
+        [gallery, page],
+        [real_img, image_orig, image, result]
     )
 
     add_text_to_image.click(
         add_text,
-        [image, page, thickness, threshold],
+        [image_orig, page, thickness, threshold],
         [image]
     )
 
@@ -150,6 +212,11 @@ with gr.Blocks() as demo:
         []
     )
 
+    add_text_from_pont.click(
+        add_text_font,
+        [image_orig, result],
+        [image]
+    )
 
 if __name__ == "__main__":
     demo.launch(share=True, server_name="0.0.0.0", server_port=5010)
